1605568f770b216da03bde74398eb2ba4ab30f9d
Alon Dotan <alon.dotan@hpe.com>
2017-05-19 15:29:12 +0300

    inital files for the exercise

---
+++ exercise/roles/getcounter/files/pandaimg-app/randpanda.js
@@ -0,0 +1,48 @@
+/* load the fs module for access the images*/
+const fs = require('fs');
+const testFolder = './resources/';
+
+var http = require('http');
+var config = require('./config.json');
+var HttpDispatcher = require('httpdispatcher');
+var dispatcher = new HttpDispatcher();
+var images = fs.readdirSync(testFolder);
+
+function handleRequest(request, response){
+    try {
+        console.log("Requested URL: " + request.url);
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+dispatcher.onGet("/", function(req, res) {
+    var rand = Math.floor(Math.random() * images.length);
+/*
+   since I used little number of images the random range is very small,
+   there for I need to make sure no duplication of rand returns that will couse duplication of results
+*/
+    if (rand == oldrand){
+        if (rand == 0){
+           rand++
+        } else {
+           ramd--
+      }
+    }
+    var img = fs.readFileSync(testFolder + images[rand]);
+    res.writeHead(200, {'Content-Type': 'image/jpg'});
+    res.end(img, 'binary');
+    var oldrand = rand;
+    console.log(rand);
+});
+
+dispatcher.onError(function(req, res) {
+        res.writeHead(404);
+        res.end("404 - Page Does not exists");
+});
+
+http.createServer(handleRequest).listen(config.port, function(){
+    console.log("Server listening on: http://localhost:%s", config.port);
+});
+
